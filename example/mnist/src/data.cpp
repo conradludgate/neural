b81341ff7d9b8482d32a095e25da207439566f32
Conrad Ludgate <oon@conradludgate.com>
2018-03-24 19:40:40 +0000

    Clean Up

---
+++ example/mnist/src/data.cpp
@@ -0,0 +1,78 @@
+#ifndef IMAGE_SIZE
+#define IMAGE_SIZE 784
+#endif
+
+#include <fstream>
+#include <Eigen/Dense>
+
+template<int A>
+using vec = Eigen::Matrix<float, A, 1>;
+
+std::ifstream train_images;
+std::ifstream train_labels;
+std::ifstream test_images;
+std::ifstream test_labels;
+
+std::ifstream* images;
+std::ifstream* labels;
+
+void load_data()
+{
+	train_images.open("data/trainingdata", std::ios::binary | std::ios::in);
+	train_labels.open("data/traininglabels", std::ios::binary | std::ios::in);
+	test_images.open("data/testingdata", std::ios::binary | std::ios::in);
+	test_labels.open("data/testinglabels", std::ios::binary | std::ios::in);
+
+	if (train_images.fail() || train_labels.fail() 
+		|| test_images.fail() || test_labels.fail())
+		exit(1);
+}
+
+void prepare_training_data()
+{
+	images = &train_images;
+	labels = &train_labels;
+}
+
+void prepare_testing_data()
+{
+	images = &test_images;
+	labels = &test_labels;
+}
+
+int get_output(vec<10> output)
+{
+	int big = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		if (output[i] > output[big])
+			big = i;
+	}
+
+	return big;
+}
+
+vec<IMAGE_SIZE> get_image(int index)
+{
+	images->seekg(16 + IMAGE_SIZE*index, std::ios::beg);
+
+	vec<IMAGE_SIZE> image;
+	uint8_t buffer [IMAGE_SIZE];
+	images->read((char*) buffer, IMAGE_SIZE);
+
+	for (int i = 0; i < IMAGE_SIZE; i++)
+	{
+		image[i] = (float)buffer[i] / 255.0;
+	}
+
+	return image;
+}
+
+int get_label(int index)
+{
+	labels->seekg(8 + index, std::ios::beg);
+
+	char x;
+	labels->read(&x, 1);
+	return x;
+}
