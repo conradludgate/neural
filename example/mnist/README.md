08954574f00e7666d8da8965bd4a0c516e1e28cc
Conrad Ludgate <oon@conradludgate.com>
2018-03-24 19:55:18 +0000

    Clean up round 2

--- example/mnist/README.md
+++ example/mnist/README.md
@@ -1,1 +1,8 @@
-README.md
+# MNIST
+
+The mnist database is a collection of handwritten numbers (ranging from 0 to 9).
+It contains 60000 training images and 10000 test images.
+
+## Usage
+First, run `make`. Then run `./mnist` and enter a learning rate, try the value `0.1` for example.
+Then wait, after a few seconds, you will see the results of the network.
--- example/mnist/src/train.cpp
+++ example/mnist/src/train.cpp
@@ -7,13 +7,16 @@
 vec<IMAGE_SIZE> get_image(int index);
 int get_label(int index);
 
+// Train for one epoch (Over all the training data available)
 void train_epoch(neural::Trainer<IMAGE_SIZE, 30, 10>& nn)
 {
 	for  (int index = 0; index < 60000; index++)
 	{
+		// Process the expected value into a vector
 		Eigen::Matrix<float, 10, 1> expected = Eigen::Matrix<float, 10, 1>::Zero();
 		expected[get_label(index)%10] = 1;
 
+		// Train
 		nn.train(get_image(index), expected);
 	}
 }
