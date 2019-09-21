48caba62f8c55a44d4fd4077f6c646eb33d26f8a
Conrad Ludgate <oon@conradludgate.com>
2019-09-21 09:27:56 +0100

    Add some documentation

--- net.hpp
+++ net.hpp
@@ -37,37 +37,45 @@ public:
 	std::ostream &operator<<(std::ostream &os) { return save<0>(os); }
 	std::istream &operator>>(std::istream &is) { return load<0>(is); }
 
+	// Train the network using a batch of inputs and their corresponding expected values
 	template <int Batch>
 	void train(Scalar lr, mat<Scalar, Inputs, Batch> input, mat<Scalar, Outputs, Batch> expected)
 	{
 		feedforward_backward(lr, input, expected);
 	}
 
+	// Predict the result given the input
 	template <int Batch>
 	mat<Scalar, Outputs, Batch> predict(const mat<Scalar, Inputs, Batch> &input)
 	{
 		if constexpr (sizeof...(HiddenLayers) == 0)
 		{
+			// If no hidden layers, just return the first layer result
 			return std::get<0>(layers).feedforward(input);
 		}
 		else
 		{
+			// Otherwise calculate the intermediate result and then feed it through to the hidden layers
 			auto output = std::get<0>(layers).feedforward(input);
 			return Net<Scalar, Cost, HiddenLayers...>(neural::pop_front(layers)).predict(output);
 		}
 	}
 
+	// The main component of training
 	template <int Batch>
 	mat<Scalar, Inputs, Batch> feedforward_backward(Scalar learning_rate, mat<Scalar, Inputs, Batch> input, mat<Scalar, Outputs, Batch> expected)
 	{
 		if constexpr (sizeof...(HiddenLayers) == 0)
 		{
+			// If no hidden layers left, get the first layer to feedforward_backward
+			// using the cost layer as it's next step
 			return std::get<0>(layers)
 				.feedforward_backward(
 					learning_rate, input, expected, CostLayer<Scalar, Cost, Outputs>());
 		}
 		else
 		{
+			// Otherwise, feedforward_backward with the remaining hiddenlayers as the next step
 			return std::get<0>(layers)
 				.feedforward_backward(
 					learning_rate, input, expected,
@@ -79,6 +87,7 @@ private:
 	template <int n>
 	void random()
 	{
+		// Set all the layers to random
 		std::get<n>(layers).random();
 		if constexpr (sizeof...(HiddenLayers) != n)
 			random<n + 1>();
@@ -87,6 +96,7 @@ private:
 	template <int n>
 	std::ostream &save(std::ostream &os)
 	{
+		// Save all the layers
 		std::get<n>(layers).save(os);
 		if constexpr (n != sizeof...(HiddenLayers))
 		{
@@ -98,6 +108,7 @@ private:
 	template <int n>
 	std::istream &load(std::istream &is)
 	{
+		// Load all the layers
 		std::get<n>(layers).load(is);
 		if constexpr (n != sizeof...(HiddenLayers))
 		{
